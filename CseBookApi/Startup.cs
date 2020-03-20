cdfce5356d3284a939feffa86060b43ba0faad9f
Nica <pereav110@gmail.com>
2020-03-21 02:31:14 +0800

    Answer Api in Memory

--- CseBookApi/Startup.cs
+++ CseBookApi/Startup.cs
@@ -1,9 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using CseBookApi.Models;
 
